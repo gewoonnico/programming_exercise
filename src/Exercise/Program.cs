70176ffb240e4c7ba34064ccae1a348f3193d06e
Nicp <lostitz0r@gmail.com>
2023-07-04 12:50:26 +0200

    Add input reading + validation
    - Allow user to exit by providing empty input
    - Validate file format
    - Validate existence of file

--- src/Exercise/Program.cs
+++ src/Exercise/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to File Reader");
+Console.WriteLine("-----");
+Console.WriteLine("Please enter the name of a text file you would like to read (leave empty to quit):");
+
+string? fileName;
+bool fileIsValid = false;
+
+do {
+    fileName = Console.ReadLine();
+
+    try {
+        testFileName(fileName);
+        fileIsValid = true;
+    } catch (ArgumentNullException) {
+        // Provide an empty input for end user to stop application
+        Console.WriteLine("Empty input detected - stopping application...");
+        break;
+    } catch (FormatException) {
+        Console.WriteLine("\nPlease provide a file name of the '.txt' format (leave empty to quit):");
+    } catch (FileNotFoundException) {
+        Console.WriteLine("\nThe file name entered does not exist, please try again (leave empty to quit):");
+    }
+} while (!fileIsValid);
+
+void testFileName(string? file) {
+    if (String.IsNullOrEmpty(file)) {
+        // Given input is empty
+        throw new ArgumentNullException();
+    }
+
+    if (!file.EndsWith(".txt")) {
+        // Given input is wrong file format
+        throw new FormatException();
+    }
+
+    if (!File.Exists(file)) {
+        // Given input does not exist on file system
+        throw new FileNotFoundException();
+    }
+}
